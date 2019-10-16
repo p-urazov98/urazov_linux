58995890bdfe324a407975f140c614ae47719fbd
p-urazov98 <pasha5310@gmail.com>
2019-10-16 20:26:34 +0300

    Third commit.

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ch;
+	cout<<"Hello World!"<<endl;
+	cout<<"Yes or No?(y/n)"<<endl;
+	cin<<сh;
+	if(ch==y)
+	{
+	cout<<"You are good boy (girl)!"<<endl;
+	}
+	else
+	{
+	cout<<"You are strangeman..."<<endl;
+	}
+	return 0;
+}
+
